d9fba6a44e6f4d4d66fbc10e2629c1e453cb2769
OrenP_BGU <80120095+oren-p-bgu@users.noreply.github.com>
2021-06-10 00:20:36 +0300

    Added jar creation, added colour to options menu

--- dev/src/main/java/PresentationLayer/OptionsMenu.java
+++ dev/src/main/java/PresentationLayer/OptionsMenu.java
@@ -22,13 +22,24 @@ public abstract class OptionsMenu {
         in = new Scanner(System.in);
     }
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     /*
     * Lists options in sorted order.
      */
     public void displayOptions(){
+
         System.out.println("\nPlease choose an option:");
         for (Map.Entry<Integer, Option> entry : options.entrySet()) {
-            System.out.println(entry.getKey() + " => " + entry.getValue().getDescription());
+            System.out.println(ANSI_GREEN + entry.getKey()+ " => "  + entry.getValue().getDescription() +  ANSI_RESET);
         }
     }
 
